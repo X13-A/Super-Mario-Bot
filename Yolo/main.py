d0c325ceff90ee9b6d31e1199bb320ebd3e229ef
Alex <foulonalex@yahoo.com>
2023-09-27 21:08:06 +0200

    Trained model with 100 Epoch

--- Yolo/main.py
+++ Yolo/main.py
@@ -3,5 +3,5 @@ from ultralytics import YOLO
 print("loading model...")
 model = YOLO("yolov8n.yaml")
 print("training model...")
-results = model.train(data="./config.yaml", epochs=1)
+results = model.train(data="./config.yaml", epochs=100)
 print("training finished !")
