b89696301ece63ed134b171c533cf0c884d9dc8b
lemagnyt <lemagnythomas@gmail.com>
2023-10-27 09:32:54 +0200

    delta enemies

--- Machine_Learning/train.py
+++ Machine_Learning/train.py
@@ -16,3 +16,15 @@ class Training():
         state, reward, self.done, truncated, info = self.env.step(self.env.action_space.sample())
         self.fitness = info["x_pos"]
         self.env.render()
+        print(self.getDeltaEnemy())
+    
+    def getDeltaEnemy(self):
+        x,y = self.smb.get_mario_location_in_level(self.ram)
+        enemies = self.smb.get_enemy_locations(self.ram)
+        dx = 16
+        dy = 16
+        if(len(enemies)>0) :
+            closest_enemy = min(enemies, key=lambda e: (e.location.x - x)**2 + (e.location.y - y)**2)
+            dx = round((closest_enemy.location.x - x)/16)
+            dy = round((closest_enemy.location.y - y)/16)
+        return (dx,dy)
